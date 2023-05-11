7bfce14d9e22c97b6c28c54292fb81b3fd35853f
Elliott Burris <elliott.burris@gmail.com>
2023-05-10 23:21:57 -0700

    trim extra lines on document

--- packages/language-server/src/formatting.ts
+++ packages/language-server/src/formatting.ts
@@ -6,9 +6,19 @@ export function formatDocument(text: string) {
   if (!hasTags) {
     text = `<Prompt>\n${text}\n</Prompt>`
   }
-  const lines = text.split('\n')
+  let lines = text.split('\n')
   const formattedLines: string[] = []
   let insertEmptyLine = false
+
+  // Trim leading empty lines
+  while (lines.length && lines[0].trim() === '') {
+    lines = lines.slice(1)
+  }
+
+  // Trim trailing empty lines
+  while (lines.length && lines[lines.length - 1].trim() === '') {
+    lines = lines.slice(0, -1)
+  }
 
   for (let index = 0; index < lines.length; index++) {
     const line = lines[index]
