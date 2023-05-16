07cca231fdba283727f1d827a9266962a6db3fa8
Elliott Burris <elliott.burris@gmail.com>
2023-05-16 14:00:04 -0700

    fixing icons

--- packages/rig/src/FileTopper.tsx
+++ packages/rig/src/FileTopper.tsx
@@ -9,36 +9,25 @@ export const FileTopper = (props: FileTopperProps) => {
 
   return (
     <div style={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center', paddingBottom: '4px' }}>
-      <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0,0,256,254.125">
-        <g transform="translate(28.0292,27.82391) scale(0.78102,0.78102)">
-          <g
-            fill="none"
-            fill-rule="nonzero"
-            stroke="#147c72"
-            stroke-width="none"
-            stroke-linecap="butt"
-            stroke-linejoin="miter"
-            stroke-miterlimit="10"
-            stroke-dasharray=""
-            stroke-dashoffset="0"
-            font-family="none"
-            font-weight="none"
-            font-size="none"
-            text-anchor="none"
-          >
-            <g transform="translate(0.93431,7.47126) scale(1.86861,1.86861)">
-              <g>
-                <rect x="4" y="0" width="128" height="128" rx="3" ry="0" stroke-width="24"></rect>
-                <path d="M104.888,96.7676l-23,24.0004" stroke-width="8"></path>
-                <path d="M129.966,51.6837l-19,21" stroke-width="8"></path>
-                <path d="M29.888,60.7676l-23.00005,24" stroke-width="8"></path>
-                <path d="M88.888,48.7676l-23,24" stroke-width="8"></path>
-                <path d="M47.888,96.7676l-23,24.0004" stroke-width="8"></path>
-                <path d="M61.888,10.7676l-16.6054,17.328" stroke-width="8"></path>
-              </g>
-            </g>
-          </g>
-        </g>
+      <svg width="20" height="20" viewBox="0 0 128 128" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <mask id="path-1-inside-1_18_47" fill="white">
+          <rect width="128" height="128" rx="3" />
+        </mask>
+        <rect width="128" height="128" rx="3" stroke="#5B8A72" stroke-width="20" mask="url(#path-1-inside-1_18_47)" />
+        <path d="M80.1388 95.7545L57.7857 118.493" stroke="#5B8A72" stroke-width="10" stroke-linecap="round" />
+        <path d="M121 49L104.022 66.2713" stroke="#5B8A72" stroke-width="10" stroke-linecap="round" />
+        <path d="M24.1778 53.9711L8.02952 70.4" stroke="#5B8A72" stroke-width="10" stroke-linecap="round" />
+        <line
+          x1="73.997"
+          y1="48.6033"
+          x2="60.0343"
+          y2="62.807"
+          stroke="#5B8A72"
+          stroke-width="10"
+          stroke-linecap="round"
+        />
+        <path d="M37.9325 85.5239L7.92683 116.047" stroke="#5B8A72" stroke-width="10" stroke-linecap="round" />
+        <path d="M46.9333 8.53333L31.791 23.9369" stroke="#5B8A72" stroke-width="10" stroke-linecap="round" />
       </svg>
       <span style={{ fontSize: '14px', fontWeight: 'bold', paddingLeft: '6px' }}>
         {file.filename.split('.glass')[0]}
