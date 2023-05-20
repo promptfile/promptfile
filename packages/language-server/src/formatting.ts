b99ebb6866933f737ffc66b302a502fb275d365c
Elliott Burris <elliott.burris@gmail.com>
2023-05-20 00:12:02 -0700

    adding formatting back

--- packages/language-server/src/formatting.ts
+++ packages/language-server/src/formatting.ts
@@ -1,15 +1,39 @@
 export function formatDocument(text: string) {
-  let lines = text.split('\n')
+  // Check if the document contains any of the required tags
+  const hasTags = /<(Prompt|User|System|Assistant|Code)/.test(text)
 
-  // Trim leading empty lines
-  while (lines.length && lines[0].trim() === '') {
-    lines = lines.slice(1)
+  // If no tags are present, wrap the entire content in <Prompt> </Prompt> tags
+  if (!hasTags) {
+    text = `<Prompt>\n${text}\n</Prompt>`
+  }
+  const lines = text.split('\n')
+  const formattedLines: string[] = []
+  let insertEmptyLine = false
+
+  for (let index = 0; index < lines.length; index++) {
+    const line = lines[index]
+    const isClosingTag = line.match(/^<\/(User|System|Assistant|Code|Prompt)/)
+    const isOpeningTag = line.match(/^<(User|System|Assistant|Code|Prompt)/)
+
+    if (isOpeningTag && insertEmptyLine) {
+      if (formattedLines[formattedLines.length - 1].trim() !== '') {
+        formattedLines.push('')
+      }
+      insertEmptyLine = false
+    }
+
+    formattedLines.push(line)
+
+    if (isClosingTag) {
+      insertEmptyLine = true
+    }
   }
 
-  // Trim trailing empty lines
-  while (lines.length && lines[lines.length - 1].trim() === '') {
-    lines = lines.slice(0, -1)
-  }
+  // Remove consecutive empty lines
+  const cleanedLines = formattedLines.filter((line, index) => {
+    if (index === 0 || index === formattedLines.length - 1) return true
+    return !(line.trim() === '' && formattedLines[index - 1].trim() === '')
+  })
 
-  return lines.join('\n')
+  return cleanedLines.join('\n').trim()
 }
