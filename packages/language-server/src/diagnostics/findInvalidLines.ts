df661b103bf5d124115d69ae8c4f08f0bac77961
Elliott Burris <elliott.burris@gmail.com>
2023-05-22 12:37:15 -0700

    fixes #139

--- packages/language-server/src/diagnostics/findInvalidLines.ts
+++ packages/language-server/src/diagnostics/findInvalidLines.ts
@@ -6,38 +6,46 @@ export function findInvalidLines(textDocument: TextDocument): Diagnostic[] {
   const lines = textDocument.getText().split('\n')
 
   let insideValidElement = 0
+  let insideFrontmatter = false
 
   for (let i = 0; i < lines.length; i++) {
     const line = lines[i].trim()
 
-    if (
-      line.startsWith('<User') ||
-      line.startsWith('<Assistant') ||
-      line.startsWith('<System') ||
-      line.startsWith('<Prompt') ||
-      line.startsWith('<Code') ||
-      line.startsWith('<Text') ||
-      line.startsWith('<Chat') ||
-      line.startsWith('<State')
-    ) {
-      insideValidElement++
-    }
+    if (line === '---') {
+      insideFrontmatter = !insideFrontmatter // Flip the flag when encountering a frontmatter delimiter
+    } else if (insideFrontmatter) {
+      continue // If we're inside frontmatter, this line is valid, so skip further checks
+    } else {
+      if (
+        line.startsWith('<User') ||
+        line.startsWith('<Assistant') ||
+        line.startsWith('<System') ||
+        line.startsWith('<Prompt') ||
+        line.startsWith('<Code') ||
+        line.startsWith('<Text') ||
+        line.startsWith('<Chat') ||
+        line.startsWith('<State')
+      ) {
+        insideValidElement++
+      }
 
-    if (
-      line.startsWith('</User>') ||
-      line.startsWith('</Assistant>') ||
-      line.startsWith('</System>') ||
-      line.startsWith('</Prompt>') ||
-      line.startsWith('</Code>') ||
-      line.startsWith('</Text>') ||
-      line.startsWith('</Chat>') ||
-      line.startsWith('</State>')
-    ) {
-      insideValidElement--
-    }
+      if (
+        line.startsWith('</User>') ||
+        line.startsWith('</Assistant>') ||
+        line.startsWith('</System>') ||
+        line.startsWith('</Prompt>') ||
+        line.startsWith('</Code>') ||
+        line.startsWith('</Text>') ||
+        line.startsWith('</Chat>') ||
+        line.startsWith('</State>')
+      ) {
+        insideValidElement--
+      }
 
-    if (!insideValidElement && isInvalidLine(line)) {
-      invalidLines.push({ line: i, start: 0, end: line.length })
+      // Only check for invalid lines if we're not inside a valid element or frontmatter
+      if (!insideValidElement && !insideFrontmatter && isInvalidLine(line)) {
+        invalidLines.push({ line: i, start: 0, end: line.length })
+      }
     }
   }
 
@@ -70,7 +78,8 @@ function isInvalidLine(line: string): boolean {
     trimmedLine.startsWith('<For ') ||
     trimmedLine.startsWith('<Completion ') ||
     trimmedLine.startsWith('<Args ') ||
-    trimmedLine.startsWith('<Block ')
+    trimmedLine.startsWith('<Block ') ||
+    trimmedLine === '---'
   ) {
     return false
   }
