b96164de8d5fd72de82ab9dc76f94f2e4a2a3d86
Shreyas <sparab22@alumni.stanford.edu>
2023-05-31 15:58:24 -0700

    finished blocks, some changes to code

--- apps/docs/src/components/Navigation.jsx
+++ apps/docs/src/components/Navigation.jsx
@@ -52,27 +52,18 @@ function NavLink({ href, tag, active, isAnchorLink = false, children }) {
 
 function VisibleSectionHighlight({ group, pathname }) {
   let [sections, visibleSections] = useInitialValue(
-    [
-      useSectionStore((s) => s.sections),
-      useSectionStore((s) => s.visibleSections),
-    ],
+    [useSectionStore(s => s.sections), useSectionStore(s => s.visibleSections)],
     useIsInsideMobileNavigation()
   )
 
   let isPresent = useIsPresent()
   let firstVisibleSectionIndex = Math.max(
     0,
-    [{ id: '_top' }, ...sections].findIndex(
-      (section) => section.id === visibleSections[0]
-    )
+    [{ id: '_top' }, ...sections].findIndex(section => section.id === visibleSections[0])
   )
   let itemHeight = remToPx(2)
-  let height = isPresent
-    ? Math.max(1, visibleSections.length) * itemHeight
-    : itemHeight
-  let top =
-    group.links.findIndex((link) => link.href === pathname) * itemHeight +
-    firstVisibleSectionIndex * itemHeight
+  let height = isPresent ? Math.max(1, visibleSections.length) * itemHeight : itemHeight
+  let top = group.links.findIndex(link => link.href === pathname) * itemHeight + firstVisibleSectionIndex * itemHeight
 
   return (
     <motion.div
@@ -80,7 +71,7 @@ function VisibleSectionHighlight({ group, pathname }) {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1, transition: { delay: 0.2 } }}
       exit={{ opacity: 0 }}
-      className="absolute inset-x-0 top-0 bg-zinc-800/2.5 will-change-transform dark:bg-white/2.5"
+      className="bg-zinc-800/2.5 dark:bg-white/2.5 absolute inset-x-0 top-0 will-change-transform"
       style={{ borderRadius: 8, height, top }}
     />
   )
@@ -89,13 +80,13 @@ function VisibleSectionHighlight({ group, pathname }) {
 function ActivePageMarker({ group, pathname }) {
   let itemHeight = remToPx(2)
   let offset = remToPx(0.25)
-  let activePageIndex = group.links.findIndex((link) => link.href === pathname)
+  let activePageIndex = group.links.findIndex(link => link.href === pathname)
   let top = offset + activePageIndex * itemHeight
 
   return (
     <motion.div
       layout
-      className="absolute left-2 h-6 w-px bg-accent-500"
+      className="bg-accent-500 absolute left-2 h-6 w-px"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1, transition: { delay: 0.2 } }}
       exit={{ opacity: 0 }}
@@ -109,39 +100,25 @@ function NavigationGroup({ group, className }) {
   // state, so that the state does not change during the close animation.
   // The state will still update when we re-open (re-render) the navigation.
   let isInsideMobileNavigation = useIsInsideMobileNavigation()
-  let [router, sections] = useInitialValue(
-    [useRouter(), useSectionStore((s) => s.sections)],
-    isInsideMobileNavigation
-  )
+  let [router, sections] = useInitialValue([useRouter(), useSectionStore(s => s.sections)], isInsideMobileNavigation)
 
-  let isActiveGroup =
-    group.links.findIndex((link) => link.href === router.pathname) !== -1
+  let isActiveGroup = group.links.findIndex(link => link.href === router.pathname) !== -1
 
   return (
     <li className={clsx('relative mt-6', className)}>
-      <motion.h2
-        layout="position"
-        className="text-xs font-semibold text-zinc-900 dark:text-white"
-      >
+      <motion.h2 layout="position" className="text-xs font-semibold text-zinc-900 dark:text-white">
         {group.title}
       </motion.h2>
       <div className="relative mt-3 pl-2">
         <AnimatePresence initial={!isInsideMobileNavigation}>
-          {isActiveGroup && (
-            <VisibleSectionHighlight group={group} pathname={router.pathname} />
-          )}
+          {isActiveGroup && <VisibleSectionHighlight group={group} pathname={router.pathname} />}
         </AnimatePresence>
-        <motion.div
-          layout
-          className="absolute inset-y-0 left-2 w-px bg-zinc-900/10 dark:bg-white/5"
-        />
+        <motion.div layout className="absolute inset-y-0 left-2 w-px bg-zinc-900/10 dark:bg-white/5" />
         <AnimatePresence initial={false}>
-          {isActiveGroup && (
-            <ActivePageMarker group={group} pathname={router.pathname} />
-          )}
+          {isActiveGroup && <ActivePageMarker group={group} pathname={router.pathname} />}
         </AnimatePresence>
         <ul role="list" className="border-l border-transparent">
-          {group.links.map((link) => (
+          {group.links.map(link => (
             <motion.li key={link.href} layout="position" className="relative">
               <NavLink href={link.href} active={link.href === router.pathname}>
                 {link.title}
@@ -160,13 +137,9 @@ function NavigationGroup({ group, className }) {
                       transition: { duration: 0.15 },
                     }}
                   >
-                    {sections.map((section) => (
+                    {sections.map(section => (
                       <li key={section.id}>
-                        <NavLink
-                          href={`${link.href}#${section.id}`}
-                          tag={section.tag}
-                          isAnchorLink
-                        >
+                        <NavLink href={`${link.href}#${section.id}`} tag={section.tag} isAnchorLink>
                           {section.title}
                         </NavLink>
                       </li>
@@ -191,9 +164,9 @@ export const navigation = [
       { title: 'VSCode extension', href: '/vscode' },
       { title: 'Languages supported', href: '/languages' },
       { title: 'Models supported', href: '/models' },
+      { title: 'Programmatic Prompting', href: '/code' },
       { title: 'Types of Blocks', href: '/blocks' },
       // { title: 'Control Logic', href: '/logic' },
-      { title: 'Code inside Prompt', href: '/code' },
       { title: 'Commenting', href: '/comments' },
       // { title: 'Token Counter', href: '/tokens' },
       { title: 'Changelog', href: '/changelog' },
@@ -220,11 +193,7 @@ export function Navigation(props) {
         <TopLevelNavItem href="#">Documentation</TopLevelNavItem>
         <TopLevelNavItem href="#">Support</TopLevelNavItem> */}
         {navigation.map((group, groupIndex) => (
-          <NavigationGroup
-            key={group.title}
-            group={group}
-            className={groupIndex === 0 && 'md:mt-0'}
-          />
+          <NavigationGroup key={group.title} group={group} className={groupIndex === 0 && 'md:mt-0'} />
         ))}
         {/* <li className="sticky bottom-0 z-10 mt-6 min-[416px]:hidden">
           <Button
