024876cdc640fa21327c57bd022783eb21d45e85
Elliott Burris <elliott.burris@gmail.com>
2023-06-01 14:23:26 -0700

    topperview updates

--- packages/glasslib/src/runGlass.ts
+++ packages/glasslib/src/runGlass.ts
@@ -164,7 +164,7 @@ ${setNextUserText}
 }
 
 const requestNodeReplacement = (model: string, message: string, streaming: boolean) => {
-  return `<Assistant model="${model}">
+  return `<Assistant model="${model}" temperature="1">
 ${message}${streaming ? '█' : ''}
 </Assistant>`
 }
