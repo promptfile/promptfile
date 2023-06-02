e5f2b95f126e3394828f0a8891784a2005ab8bdc
Elliott Burris <elliott.burris@gmail.com>
2023-06-01 17:18:33 -0700

    moving tokenCounter

--- apps/vscode-glass/src/util/tokenCounter.ts
+++ apps/vscode-glass/src/util/tokenCounter.ts
@@ -1,9 +1,5 @@
-import cl100k_base from '@dqbd/tiktoken/encoders/cl100k_base.json'
-import { Tiktoken } from '@dqbd/tiktoken/lite'
-import { parseGlassBlocks } from '@glass-lang/glasslib'
+import { countTokens, parseGlassBlocks } from '@glass-lang/glasslib'
 import * as vscode from 'vscode'
-
-const encoding = new Tiktoken(cl100k_base.bpe_ranks, cl100k_base.special_tokens, cl100k_base.pat_str)
 
 export function updateTokenCount(counter: vscode.StatusBarItem) {
   const editor = vscode.window.activeTextEditor
@@ -15,16 +11,17 @@ export function updateTokenCount(counter: vscode.StatusBarItem) {
       block => block.tag && ['System', 'User', 'Assistant', 'Block'].includes(block.tag)
     )
     const promptText = promptBlocks.map(block => block.child?.content ?? '').join('\n\n')
+    console.log(promptText)
     try {
-      const fullTextTokens = encoding.encode(promptText)
+      const fullTextTokens = countTokens(promptText)
       let selectedTextTokensCount = 0
 
-      const counterParts: string[] = [`${fullTextTokens.length} prompt token${fullTextTokens.length === 1 ? '' : 's'}`]
+      const counterParts: string[] = [`${fullTextTokens} prompt token${fullTextTokens === 1 ? '' : 's'}`]
 
       // Check if there is a selection
       if (!editor.selection.isEmpty) {
         const selectedText = document.getText(editor.selection)
-        selectedTextTokensCount = encoding.encode(selectedText).length
+        selectedTextTokensCount = countTokens(selectedText)
         counterParts.push(`(${selectedTextTokensCount} selected)`)
       }
 
