b380b342304341367dbc740120d0a1bd2ae837a3
John Rothfels <john.rothfels@gmail.com>
2023-06-05 10:25:18 -0700

    remove old docs

--- apps/docs/src/util/fetchEmbeddings.ts
+++
@@ -1,3 +0,0 @@
-export function fetchEmbeddings(queryString: string, namespace: string) {
-  return `yay embeddings! this is just a demo with queryString: ${queryString} and namespace: ${namespace}`
-}
