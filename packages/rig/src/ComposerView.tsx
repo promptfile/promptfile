ed97619b9d6745f699f2c35e5da608ae1255a34f
Elliott Burris <elliott.burris@gmail.com>
2023-06-05 10:52:12 -0700

    better composer view in no variable situation

--- packages/rig/src/ComposerView.tsx
+++ packages/rig/src/ComposerView.tsx
@@ -33,50 +33,56 @@ export const ComposerView = (props: ComposerViewProps) => {
           paddingRight: '16px',
         }}
       >
-        <div
-          style={{
-            width: '100%',
-            display: 'flex',
-            flexDirection: 'column',
-            paddingRight: '8px',
-          }}
-        >
-          {keys.map((key, index) => (
-            <div key={key} style={{ width: '100%' }}>
-              {keys.length > 1 && (
-                <div style={{ paddingTop: index === 0 ? '0px' : '8px', paddingBottom: '4px' }}>{key}</div>
-              )}
-              <VSCodeTextArea
-                style={{ width: '100%' }}
-                value={inputs[key]}
-                id={`composer-input-${index}`}
-                placeholder={keys.length === 1 ? key : ''}
-                onInput={e => {
-                  const value = (e.target as any).value
-                  setInputs({ ...inputs, [key]: value })
-                }}
-                onKeyDown={e => {
-                  if (e.metaKey && e.key === 'r') {
-                    e.preventDefault()
-                    reload()
-                  } else if (e.metaKey && e.key === 'Enter') {
-                    e.preventDefault()
-                    reload()
-                  } else if (!streaming && e.key === 'Enter' && !e.shiftKey) {
-                    e.preventDefault()
-                    run(inputs)
-                  }
-                }}
-              />
-            </div>
-          ))}
-        </div>
+        {keys.length > 0 && (
+          <div
+            style={{
+              width: '100%',
+              display: 'flex',
+              flexDirection: 'column',
+              paddingRight: '8px',
+            }}
+          >
+            {keys.map((key, index) => (
+              <div key={key} style={{ width: '100%' }}>
+                {keys.length > 1 && (
+                  <div style={{ paddingTop: index === 0 ? '0px' : '8px', paddingBottom: '4px' }}>{key}</div>
+                )}
+                <VSCodeTextArea
+                  style={{ width: '100%' }}
+                  value={inputs[key]}
+                  id={`composer-input-${index}`}
+                  placeholder={keys.length === 1 ? key : ''}
+                  onInput={e => {
+                    const value = (e.target as any).value
+                    setInputs({ ...inputs, [key]: value })
+                  }}
+                  onKeyDown={e => {
+                    if (e.metaKey && e.key === 'r') {
+                      e.preventDefault()
+                      reload()
+                    } else if (e.metaKey && e.key === 'Enter') {
+                      e.preventDefault()
+                      reload()
+                    } else if (!streaming && e.key === 'Enter' && !e.shiftKey) {
+                      e.preventDefault()
+                      run(inputs)
+                    }
+                  }}
+                />
+              </div>
+            ))}
+          </div>
+        )}
         {streaming ? (
-          <VSCodeButton appearance="secondary" onClick={stop}>
+          <VSCodeButton style={{ width: keys.length === 0 ? '100%' : undefined }} appearance="secondary" onClick={stop}>
             Stop
           </VSCodeButton>
         ) : (
-          <VSCodeButton appearance="primary" onClick={() => run(inputs)}>
+          <VSCodeButton
+            style={{ width: keys.length === 0 ? '100%' : undefined }}
+            appearance="primary"
+            onClick={() => run(inputs)}
+          >
             Run
           </VSCodeButton>
         )}
