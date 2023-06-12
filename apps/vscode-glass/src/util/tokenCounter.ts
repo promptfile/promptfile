c048adaac82e8bbb6d733770e572eaf826955c82
Elliott Burris <elliott.burris@gmail.com>
2023-06-11 23:49:01 -0700

    don't show token counter unless on glass file

--- apps/vscode-glass/src/util/tokenCounter.ts
+++ apps/vscode-glass/src/util/tokenCounter.ts
@@ -39,32 +39,34 @@ export function maxTokensForModel(model: string) {
 
 export function updateTokenCount(counter: vscode.StatusBarItem) {
   const editor = vscode.window.activeTextEditor
-  if (editor) {
-    const document = editor.document
-    const fullText = document.getText()
-    const initialBlocks = parseGlassBlocks(fullText)
-    const promptBlocks = initialBlocks.filter(
-      block => block.tag && ['System', 'User', 'Assistant', 'Block'].includes(block.tag)
-    )
-    const promptText = promptBlocks.map(block => block.child?.content ?? '').join('\n\n')
-    try {
-      const fullTextTokens = countTokens(promptText)
-      let selectedTextTokensCount = 0
+  if (!editor) {
+    counter.hide()
+    return
+  }
+  const document = editor.document
+  const fullText = document.getText()
+  const initialBlocks = parseGlassBlocks(fullText)
+  const promptBlocks = initialBlocks.filter(
+    block => block.tag && ['System', 'User', 'Assistant', 'Block'].includes(block.tag)
+  )
+  const promptText = promptBlocks.map(block => block.child?.content ?? '').join('\n\n')
+  try {
+    const fullTextTokens = countTokens(promptText)
+    let selectedTextTokensCount = 0
 
-      const counterParts: string[] = [`${fullTextTokens} prompt token${fullTextTokens === 1 ? '' : 's'}`]
+    const counterParts: string[] = [`${fullTextTokens} prompt token${fullTextTokens === 1 ? '' : 's'}`]
 
-      // Check if there is a selection
-      if (!editor.selection.isEmpty) {
-        const selectedText = document.getText(editor.selection)
-        selectedTextTokensCount = countTokens(selectedText)
-        counterParts.push(`(${selectedTextTokensCount} selected)`)
-      }
-
-      counter.text = counterParts.join(' ')
-      counter.show()
-    } catch {
-      // TODO: handle error
+    // Check if there is a selection
+    if (!editor.selection.isEmpty) {
+      const selectedText = document.getText(editor.selection)
+      selectedTextTokensCount = countTokens(selectedText)
+      counterParts.push(`(${selectedTextTokensCount} selected)`)
     }
+
+    counter.text = counterParts.join(' ')
+    counter.show()
+  } catch {
+    counter.hide()
   }
 }
 
