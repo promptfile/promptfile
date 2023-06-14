decbf9f5ba499f868ac54febc02a4d558176c85e
Elliott Burris <elliott.burris@gmail.com>
2023-06-14 16:51:46 -0700

    fixes #413

--- packages/rig/src/ComposerView.tsx
+++ packages/rig/src/ComposerView.tsx
@@ -1,7 +1,7 @@
 import MonacoEditor from '@monaco-editor/react'
 import { VSCodeButton, VSCodeDivider } from '@vscode/webview-ui-toolkit/react'
-
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import { firstElement } from './util'
 
 interface ComposerViewProps {
   theme: string
@@ -21,6 +21,14 @@ export const ComposerView = (props: ComposerViewProps) => {
   const sessionRef = useRef(session)
 
   const keys: string[] = Object.keys(inputs)
+  const firstKey = firstElement(keys)
+  const [activeKey, setActiveKey] = useState(firstKey ?? '')
+
+  useEffect(() => {
+    if (!keys.includes(activeKey)) {
+      setActiveKey(firstElement(keys) ?? '')
+    }
+  }, [keys])
 
   useEffect(() => {
     document.getElementById('composer-input-0')?.focus()
@@ -63,39 +71,72 @@ export const ComposerView = (props: ComposerViewProps) => {
               width: '100%',
               display: 'flex',
               flexDirection: 'column',
-              paddingRight: '8px',
             }}
           >
-            {keys.map((key, index) => (
-              <div key={key} style={{ width: '100%' }}>
-                <div style={{ paddingTop: index === 0 ? '0px' : '8px', paddingBottom: '4px' }}>{key}</div>
-                <MonacoEditor
-                  width="100%"
-                  height={`100px`}
-                  theme={mapVSCodeThemeToMonaco(theme)}
-                  language={'markdown'}
-                  value={inputs[key]}
-                  onChange={value => setValue(key, value ?? '')}
-                  options={{
-                    minimap: {
-                      enabled: false,
-                    },
-                    padding: {
-                      top: 4,
-                    },
-                    wordWrap: 'on',
-                    fontSize: 12,
-                    lineDecorationsWidth: 0,
-                  }}
-                  onMount={(editor, monaco) => {
-                    editor.focus()
-                    editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.Enter, () => {
-                      run(inputsRef.current, sessionRef.current)
-                    })
-                  }}
-                />
-              </div>
-            ))}
+            <div style={{ display: 'flex', paddingLeft: '8px' }}>
+              {keys.map(key => {
+                const isCurrentTab = key === activeKey
+                const opacity = isCurrentTab ? 1 : 0.5
+                const color = isCurrentTab ? 'white' : undefined
+                const borderBottomColor = isCurrentTab ? 'white' : 'transparent'
+                return (
+                  <div style={{ paddingRight: '24px' }} key={key}>
+                    <div
+                      style={{
+                        opacity,
+                        color,
+                        borderBottomStyle: 'solid',
+                        borderBottomWidth: '2px',
+                        borderBottomColor,
+                        fontSize: '12px',
+                        cursor: 'pointer',
+                        paddingBottom: '4px',
+                        paddingTop: '4px',
+                        paddingLeft: '8px',
+                        paddingRight: '8px',
+                      }}
+                      onClick={() => setActiveKey(key)}
+                      onMouseEnter={(event: any) => {
+                        event.target.style.opacity = '1.0'
+                      }}
+                      onMouseLeave={(event: any) => {
+                        event.target.style.opacity = opacity
+                      }}
+                    >
+                      {key}
+                    </div>
+                  </div>
+                )
+              })}
+            </div>
+            {activeKey.length > 0 && (
+              <MonacoEditor
+                key={activeKey}
+                width="100%"
+                height={`150px`}
+                theme={mapVSCodeThemeToMonaco(theme)}
+                language={'markdown'}
+                value={inputs[activeKey]}
+                onChange={value => setValue(activeKey, value ?? '')}
+                options={{
+                  minimap: {
+                    enabled: false,
+                  },
+                  padding: {
+                    top: 4,
+                  },
+                  wordWrap: 'on',
+                  fontSize: 12,
+                  lineDecorationsWidth: 0,
+                }}
+                onMount={(editor, monaco) => {
+                  editor.focus()
+                  editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.Enter, () => {
+                    run(inputsRef.current, sessionRef.current)
+                  })
+                }}
+              />
+            )}
           </div>
         )}
       </div>
