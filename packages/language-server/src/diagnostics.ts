cf711738b9e354d62fdb01ce6743c3a273115753
Elliott Burris <elliott.burris@gmail.com>
2023-06-15 22:34:25 -0700

    cleaning up old code

--- packages/language-server/src/diagnostics.ts
+++ packages/language-server/src/diagnostics.ts
@@ -1,4 +1,3 @@
-import { parseFrontmatterFromGlass } from '@glass-lang/glassc'
 import { LANGUAGE_MODELS, LanguageModelCreator, LanguageModelType, parseGlassBlocks } from '@glass-lang/glasslib'
 import { Diagnostic, DiagnosticSeverity } from 'vscode-languageserver'
 import { TextDocument } from 'vscode-languageserver-textdocument'
@@ -12,7 +11,7 @@ export function getDiagnostics(textDocument: TextDocument): Diagnostic[] {
     ...findModelDiagnostics(textDocument),
     ...findRequestModelDiagnostics(textDocument),
     ...findEmptyBlocksDiagnostics(textDocument),
-    ...findFrontmatterDiagnostics(textDocument),
+    // ...findFrontmatterDiagnostics(textDocument),
     ...findMultipleTranscriptDiagnostics(textDocument),
   ]
 }
@@ -96,35 +95,35 @@ function findEmptyBlocksDiagnostics(textDocument: TextDocument): Diagnostic[] {
   }
 }
 
-function findFrontmatterDiagnostics(textDocument: TextDocument): Diagnostic[] {
-  try {
-    // get the range of the frontmatter
-    const regex = /---\n([\s\S]*?)\n---/
-    const match = regex.exec(textDocument.getText())
-    if (!match) {
-      return []
-    }
-    const range = {
-      start: textDocument.positionAt(match.index),
-      end: textDocument.positionAt(match.index + match[0].length),
-    }
-    const frontmatter = parseFrontmatterFromGlass(textDocument.getText()) as any | null
-    const diagnostics: Diagnostic[] = []
-    if (frontmatter) {
-      if (frontmatter.language && !['python', 'javascript', 'typescript'].includes(frontmatter.language)) {
-        diagnostics.push({
-          severity: DiagnosticSeverity.Error,
-          range,
-          message: `Unsupported language: ${frontmatter.language}`,
-          source: 'glass',
-        })
-      }
-    }
-    return diagnostics
-  } catch {
-    return []
-  }
-}
+// function findFrontmatterDiagnostics(textDocument: TextDocument): Diagnostic[] {
+//   try {
+//     // get the range of the frontmatter
+//     const regex = /---\n([\s\S]*?)\n---/
+//     const match = regex.exec(textDocument.getText())
+//     if (!match) {
+//       return []
+//     }
+//     const range = {
+//       start: textDocument.positionAt(match.index),
+//       end: textDocument.positionAt(match.index + match[0].length),
+//     }
+//     const frontmatter = parseFrontmatterFromGlass(textDocument.getText()) as any | null
+//     const diagnostics: Diagnostic[] = []
+//     if (frontmatter) {
+//       if (frontmatter.language && !['python', 'javascript', 'typescript'].includes(frontmatter.language)) {
+//         diagnostics.push({
+//           severity: DiagnosticSeverity.Error,
+//           range,
+//           message: `Unsupported language: ${frontmatter.language}`,
+//           source: 'glass',
+//         })
+//       }
+//     }
+//     return diagnostics
+//   } catch {
+//     return []
+//   }
+// }
 
 function findRequestModelDiagnostics(textDocument: TextDocument): Diagnostic[] {
   try {
