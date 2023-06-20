9973103580873100ea592cc07d2449ee6b51ec5a
Shreyas Parab <sparab22@stanford.edu>
2023-06-19 18:12:53 -0700

    Shreyas docs updates (#470)

--- apps/docs.glass/next.config.js
+++ apps/docs.glass/next.config.js
@@ -2,11 +2,17 @@ const shiki = require('shiki')
 const fs = require('fs')
 const path = require('path')
 
+const vscodePackageJson = JSON.parse(fs.readFileSync(path.join(__dirname, '../vscode-glass/package.json')).toString())
+
+const colors = vscodePackageJson.contributes?.configurationDefaults['editor.tokenColorCustomizations'].textMateRules
+
 const glass = JSON.parse(
   fs.readFileSync(path.join(__dirname, '../vscode-glass/syntaxes/glass.tmLanguage.json')).toString()
 )
 
 const syntaxTheme = JSON.parse(fs.readFileSync(path.join(__dirname, 'shiki-theme.json')).toString())
+
+syntaxTheme.tokenColors = [...colors, ...syntaxTheme.tokenColors]
 
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
