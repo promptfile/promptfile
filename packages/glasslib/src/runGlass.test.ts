9960d79d33ceba47be4fe3db337515cbd1aa9cfc
John Rothfels <john.rothfels@gmail.com>
2023-06-19 17:12:00 -0700

    update runGlass test case

--- packages/glasslib/src/runGlass.test.ts
+++ packages/glasslib/src/runGlass.test.ts
@@ -16,4 +16,26 @@ You are HaikuGPT. Always respond to the user in the form of a haiku.
     const res = await runGlass(doc, { input: 'Tell me about the ocean.' }, { id: () => ++id + '' })
     expect(res.response).to.deep.equal([{}])
   })
+
+  it('should run a glass document', async () => {
+    const doc = `---
+model: gpt-3.5-turbo
+---
+
+<User id="28992368998104064">
+hi!
+</User>`
+    let id = 0
+    const res = await runGlass(
+      doc,
+      {},
+      {
+        id: () => ++id + '',
+        progress: data => {
+          console.log(JSON.stringify(data.response, null, 2))
+        },
+      }
+    )
+    expect(res.response).to.deep.equal([{}])
+  })
 })
