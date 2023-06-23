d4306f4d196bef6828bad1dd8bb70576a0f308c8
Elliott Burris <elliott.burris@gmail.com>
2023-06-23 00:51:08 -0700

    cleanup on frontmatter

--- packages/glasslib/src/parseFrontmatterFromGlass.ts
+++ packages/glasslib/src/parseFrontmatterFromGlass.ts
@@ -1,12 +1,7 @@
 import { parse } from 'yaml'
 
 interface ParsedResult {
-  file: string
-  timestamp: string
   model: string
-  session: string
-  language: string
-  args: Record<string, string>
 }
 
 export function parseFrontmatterFromGlass(glass: string): ParsedResult | null {
@@ -32,15 +27,6 @@ export function parseFrontmatter(yamlString: string): ParsedResult | null {
 
   if (typeof result === 'object' && result !== null) {
     const res: any = {}
-    if (result.session) {
-      res['session'] = result.session
-    }
-    if (result.timestamp) {
-      res['timestamp'] = result.timestamp
-    }
-    if (result.file) {
-      res['file'] = result.file
-    }
     if (result.model) {
       res['model'] = result.model
     }
