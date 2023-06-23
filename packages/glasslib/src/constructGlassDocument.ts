4b6de2b5bbf5873485b17403b66fdfea0ae76ea0
Elliott Burris <elliott.burris@gmail.com>
2023-06-23 08:30:26 -0700

    adding attribute string

--- packages/glasslib/src/constructGlassDocument.ts
+++ packages/glasslib/src/constructGlassDocument.ts
@@ -3,7 +3,17 @@ import { ChatBlock } from './parseChatBlocks'
 export function constructGlassDocument(blocks: ChatBlock[], model: string) {
   const blocksAsTags = blocks.map(b => {
     const capitalizedRole = b.role[0].toUpperCase() + b.role.slice(1)
-    return `<${capitalizedRole}>\n${b.content}\n</${capitalizedRole}>`
+    const attributesToAdd: Record<string, string> = {}
+    if (b.name != null) {
+      attributesToAdd.name = b.name
+    }
+    if (b.type != null) {
+      attributesToAdd.type = b.type
+    }
+    const attributeString = Object.entries(attributesToAdd)
+      .map(([k, v]) => `${k}="${v}"`)
+      .join(' ')
+    return `<${capitalizedRole}${attributeString}>\n${b.content}\n</${capitalizedRole}>`
   })
   const tagsAsString = blocksAsTags.join('\n\n\n')
   return `---
